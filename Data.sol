6b5d4e180e09bfcd81a10e495a86504b15e47868
Linus Shyu <83543818+Linus-Shyu@users.noreply.github.com>
2023-05-20 18:50:58 +0800

    Create Data.sol
    3 class

---
+++ Data.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Data {
+    bool Linus1 = true;
+
+    uint Linus2 = 123;
+    int Linus3 = -123;
+
+    address public Linus4 = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
+    bytes1 private Linus5 = 0x33;
+
+    
+}
